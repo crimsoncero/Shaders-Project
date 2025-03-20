1f21afc1f78665faa4ab4c748dc6a4aa68352b04
Amit Breiman <crimson.breiman@gmail.com>
2025-03-20 23:36:10 +0200

    1

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
+
+public enum VerticalEnum
+{
+    None,
+    Up,
+    Down,
+}
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private TerrainModifier _terrainModifier;
+    [FormerlySerializedAs("moveSpeed")] [SerializeField] private float _moveSpeed = 7.5f;
+    [FormerlySerializedAs("lookSpeed")] [SerializeField] private float _lookSpeed = 2.5f;
+    
+    
+    [FormerlySerializedAs("isControllingCamera")] [SerializeField] private bool _isControllingCamera = false;
+    private Vector3 _moveDirection = Vector3.zero;
+    private VerticalEnum _verticalMovement = VerticalEnum.None; 
+
+    private void Update()
+    {
+        Vector3 v = transform.rotation * _moveDirection;
+        transform.position += v * _moveSpeed * Time.deltaTime;
+
+        switch (_verticalMovement)
+        {
+            case VerticalEnum.Up:
+                transform.position += Vector3.up * _moveSpeed * Time.deltaTime;
+                break;
+            case VerticalEnum.Down:
+                transform.position += Vector3.down * _moveSpeed * Time.deltaTime;
+                break;
+            case VerticalEnum.None:
+            default:
+                break;
+        }
+        
+    }
+
+    public void OnPaint(InputAction.CallbackContext context)
+    {
+        _terrainModifier.OnPaint();
+    }
+    
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Vector2 moveVec = context.ReadValue<Vector2>();
+        _moveDirection.x = moveVec.x;
+        _moveDirection.z = moveVec.y;
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        if (_isControllingCamera)
+        {
+            Vector2 lookVec = context.ReadValue<Vector2>();
+            Vector3 eulerAngles = transform.eulerAngles;
+            eulerAngles.y += lookVec.x * _lookSpeed;
+            eulerAngles.x -= lookVec.y * _lookSpeed;
+            
+            transform.eulerAngles = eulerAngles;
+        }
+    }
+
+    public void OnMoveUp(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            _verticalMovement = VerticalEnum.Up;
+        }
+        else
+        {
+            _verticalMovement = VerticalEnum.None;
+        }
+    }
+
+    public void OnMoveDown(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            _verticalMovement = VerticalEnum.Down;
+        }
+        else
+        {
+            _verticalMovement = VerticalEnum.None;
+        }
+    }
+    
+    public void OnControlCamera(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            _isControllingCamera = true;
+        }
+        else
+        {
+            _isControllingCamera = false;
+        }
+    }
+    
+}
---
+++ Assets/_Scripts/TerrainModifier.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BrushModeEnum
+{
+    HeightIncrease,
+    HeightDecrease,
+    Color,
+}
+
+public class TerrainModifier : MonoBehaviour
+{
+    [SerializeField] private Material _material;
+
+    [SerializeField] private int _brushRadius = 10;
+    [SerializeField] private float _heightChange = 0.2f;
+
+    private Texture2D _heightTexture;
+    private Texture2D _colorTexture;
+    
+    
+    
+    [field: SerializeField] public BrushModeEnum BrushMode { get; set; } = BrushModeEnum.HeightIncrease;
+
+    private void Start()
+    {
+        _heightTexture = (Texture2D)_material.GetTexture("_HeightTex");
+        _colorTexture = (Texture2D)_material.GetTexture("_ColorTex");
+    }
+
+    public void OnPaint()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        
+        if (Physics.Raycast(ray, out hit))
+        {
+            Vector2 uv = hit.textureCoord;
+            
+            switch (BrushMode)
+            {
+                case BrushModeEnum.HeightIncrease:
+                    OnHeightChange(uv,true);
+                    break;
+                case BrushModeEnum.HeightDecrease:
+                    OnHeightChange(uv,false);
+                    break;
+                case BrushModeEnum.Color:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+           
+        }
+        else
+        {
+            return;
+        }
+        
+        
+        
+        
+    }
+    private void OnHeightChange(Vector2 uvPos, bool isIncrease)
+    {
+        Vector2 pixelPos = UVToPixel(uvPos, _heightTexture);
+        
+        float change = Mathf.Lerp(0, 255, _heightChange);
+        change = isIncrease ? change : -change;
+
+
+        for (int i = -_brushRadius / 2; i < _brushRadius / 2; i++)
+        {
+            for (int j = -_brushRadius / 2; j < _brushRadius / 2; j++)
+            {
+                var pixel = _heightTexture.GetPixel((int)pixelPos.x + i, (int)pixelPos.y + j);
+                pixel.r = Mathf.Clamp(pixel.r + change, 0, 255);
+                pixel.g = Mathf.Clamp(pixel.g + change, 0, 255);
+                pixel.b = Mathf.Clamp(pixel.b + change, 0, 255);
+                _heightTexture.SetPixel((int)pixelPos.x + i, (int)pixelPos.y + j, pixel);
+            }
+        }
+        _heightTexture.Apply();
+
+    }
+
+    private Vector2 UVToPixel(Vector2 uvPos, Texture2D texture)
+    {
+        Vector2 v = Vector2.zero;
+        
+        v.x = Mathf.Lerp(0, texture.width, uvPos.x);
+        v.y = Mathf.Lerp(0, texture.height, uvPos.y);
+
+        return v;
+    }
+    
+}
